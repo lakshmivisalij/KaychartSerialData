efb93f61d2733b12ba4c2234950d36c07da707f0
Visali <Visali@DESKTOP-UUDDTG7>
2020-03-31 01:09:50 +0530

    Add project files.

---
+++ KayChartExample/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using rtChart;
+using System.IO.Ports;
+
+namespace KayChartExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            System.IO.Ports.SerialPort.GetPortNames();
+            serialPort1.DataReceived += Serial_DataReceived;
+        }
+        kayChart kc;
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {          
+            string serialData = serialPort1.ReadLine();
+         
+            kc.TriggeredUpdate(Convert.ToDouble(serialData));
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort1.Open();
+            string serialData = serialPort1.ReadLine();
+            serialData.Trim().ToString();
+          
+            kc = new kayChart(chart1, 60);
+            kc.TriggeredUpdate(Convert.ToDouble(serialData));  
+        }
+    }
+}
